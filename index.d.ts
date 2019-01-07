3385e9273a432dba77f193e039b33f6a060dec5d
jackstrapp <sherman91strike@hotmail.fr>
2019-01-07 16:33:13 -0500

    add loaded promise in ts definition

--- index.d.ts
+++ index.d.ts
@@ -9,6 +9,11 @@ declare class Raclette {
     getItem(key: string): Promise<any>;
     setItem(key: string, value: string): Promise<any>;
     clear(): Promise<any>;
+    /**
+     * the initial promise that tell if the iframe has done its loading.
+     * As it's a promise, can be called multiple times, before and after the actual initialization.
+     */
+    loaded: Promise<any>;
 }
 
 export default Raclette
